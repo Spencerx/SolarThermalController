cf0d4d25a970dbc69c4e26e5a4de38f77f1976a2
olmo <olmo+gitlab@mezgers.com>
2017-10-15 17:11:16 -0700

    starting to play arround. compiles ok

--- SolarThermalControler.ino
+++ SolarThermalControler.ino
@@ -1,6 +1,26 @@
+#include "config.h"
+
+int powerOnDelayNetwork = INITIALIZE_DELAY_NETWORK_MINUTES;
+bool doDebug = DO_DEBUG_MESSAGES;
+
 void setup() {
   // put your setup code here, to run once:
+  // first thing is to stop
+  Serial.begin(57600);
+  pinMode(RELAIS_PIN, OUTPUT);
+  if (doDebug){
+    Serial.println("Arduino started. ");
+    if(powerOnDelayNetwork>0){
+        Serial.print("Initializing delay of ");
+        Serial.print(powerOnDelayNetwork);
+        Serial.println(" Minutes to wait for network.");
+    }else{
+      Serial.println("Not waiting for network");
+    }
+  }// end of doDebug
 
+
+  delay(powerOnDelayNetwork * 1000 * 60); // in minutes!
 }
 
 void loop() {
---
+++ config.h
@@ -0,0 +1,14 @@
+#define DO_DEBUG_MESSAGES 0 // Controlls if debugging messages are shown on the serial terminal
+
+#define RELAIS_PIN 7 // the output 5V that controlls the relais
+
+#define CONTROLLER_ID "SolTh01" // the id of the contorller. this will be used to identify this controller
+
+#define USE_SIMULATOR 1 // 1: turn on the simulator, for debugging and development purposses. Normal operation is 0
+
+#define POWER_ON_HOUR 4 // the hour of the day where the electric heater has allowance to operate.
+#define POWER_ON_MINUTE 30 // the Minute of the day where the electric heater has allowance to operate.
+#define POWER_OFF_HOUR 6 // the hour of the day where the electric heater stops operating.
+#define POWER_OFF_MINUTE 30 // the Minute of the day where the electric heater stops operating.
+
+#define INITIALIZE_DELAY_NETWORK_MINUTES 1 // after a power off, wait this amount of minutes for the network to start
